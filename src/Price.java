fa9db85e414c42edba0fe5265cdb8529049480ad
Patrick <pzagiel@ImacPatrick-6.local>
2017-06-25 13:30:29 +0200

    add method computeEvol

--- src/Price.java
+++ src/Price.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Created by IntelliJ IDEA.
  * User: pzagiel
@@ -10,4 +12,14 @@ public class Price {
     public String date;
     public double priceValue;
     public double priceValueEvol;
+
+    public static ArrayList<Price> computeEvol(ArrayList<Price> prices) {
+        for (int i = prices.size() - 1; i > 0; i--) {
+            Price currentPrice = prices.get(i);
+            Price prevPrice = prices.get(i - 1);
+            currentPrice.priceValueEvol = (currentPrice.priceValue - prevPrice.priceValue) / prevPrice.priceValue;
+        }
+        return prices;
+    }
+
 }
