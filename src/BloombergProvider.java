b2dc3dbc3ddafe426bcefa57f32fc5fd386be7f5
pzagiel <patrick.zagiel@gmail.com>
2018-03-26 18:46:39 +0200

    correction of evol for historical prices

--- src/BloombergProvider.java
+++ src/BloombergProvider.java
@@ -1,19 +1,17 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
-import org.jsoup.Jsoup;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.JSONObject;
-import org.json.simple.JSONArray;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.HashMap;
-import java.text.DecimalFormatSymbols;
-import java.text.DecimalFormat;
-import java.text.SimpleDateFormat;
-import java.text.ParseException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,9 +23,14 @@ import java.text.ParseException;
 public class BloombergProvider {
 
     public static void main(String[] args) {
-        BloombergProvider bloombergProvider = new BloombergProvider();
 
-       bloombergProvider.getLastPrice("IL0011213001", "SODA:US");  //Sodastream
+        BloombergProvider bloombergProvider = new BloombergProvider();
+        //bloombergProvider.getPrices("SPX", "SPX:IND");
+        bloombergProvider.getLastPrice("SPX", "SPX:IND");  // S&P 500 Index
+        bloombergProvider.getLastPrice("US47215P1066", "JD:US");  // JD.COM
+        bloombergProvider.getLastPrice("NL0000226223", "STM:FP");  // STMicroelectronics NV
+        bloombergProvider.getLastPrice("FR0010923375", "NH2MBRC:FP");  // H20 Multibonds
+        bloombergProvider.getLastPrice("IL0011213001", "SODA:US");  //Sodastream
         bloombergProvider.getLastPrice("US01609W1027", "BABA:US");  //Alibaba
         bloombergProvider.getLastPrice("US98978V1035", "ZTS:US");  //Zoetis Inc
         bloombergProvider.getLastPrice("CL1COM", "CL1:COM");  //WTI Crude
@@ -35,8 +38,8 @@ public class BloombergProvider {
         bloombergProvider.getLastPrice("NL0011821202", "INGA:NA");  // ING
         bloombergProvider.getLastPrice("LU0955120620", "RORIHUS:LX");
         bloombergProvider.getLastPrice("IE00B1FZSC47", "IDTP:LN");  //iShares $ TIPS UCITS ETF (USD) | ITPS
-        bloombergProvider.getLastPrice("LU0234577681", "PIWEQUT:LX");  //Piguet International Fund World Equities D (USD)
-        bloombergProvider.getLastPrice("LU0334117230", "PIWEDED:LX");  //Piguet International Fund World Equities D (EUR)
+        // bloombergProvider.getLastPrice("LU0234577681", "PIWEQUT:LX");  //Piguet International Fund World Equities D (USD)
+        //bloombergProvider.getLastPrice("LU0334117230", "PIWEDED:LX");  //Piguet International Fund World Equities D (EUR)
         bloombergProvider.getLastPrice("US0378331005", "AAPL:US");  //Apple Inc
         bloombergProvider.getLastPrice("GB00B0CTWC01", "OLBP:LN");  // ETFS WTI Crude Oil USD
         bloombergProvider.getLastPrice("GB00B15KXV33", "CRUD:LN");  // Brent ETFS Tracker in GBX
@@ -80,7 +83,7 @@ public class BloombergProvider {
 
     public void getPrices(String isin, String ticker) {
         // Possible to use 1_WEEK, 1_MONTH, 1_YEAR,5_YEAR,MAX default is 1_WEEK
-        String baseUrl = "http://www.bloomberg.com/markets/api/bulk-time-series/price/" + ticker + "?timeFrame=1_MONTH";
+        String baseUrl = "http://www.bloomberg.com/markets/api/bulk-time-series/price/" + ticker + "?timeFrame=1_YEAR";
         Document doc = null;
         Elements priceElt = null;
         try {
@@ -91,7 +94,7 @@ public class BloombergProvider {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
 
-        Price priceValue = new Price();
+
         JSONParser parser = new JSONParser();
         Object obj = null;
         try {
@@ -101,8 +104,9 @@ public class BloombergProvider {
 
             ArrayList prices1 = (ArrayList) jsonPrice.get("price");
             // Loop on prices
-
+            ArrayList priceValues = new ArrayList();
             for (int i = 0; i < prices1.size(); i++) {
+                Price priceValue = new Price();
                 priceValue.instrumentCode = isin;
                 priceValue.provider = "Bloomberg";
                 JSONObject jsonPrice1 = (JSONObject) prices1.get(i);
@@ -131,9 +135,11 @@ public class BloombergProvider {
                 }
                 if (priceValue != null)
                     System.out.println("Store Price From Bloomberg " + priceValue.instrumentCode + " " + priceValue.date + " " + priceValue.priceValue);
-                new StorePrice().storeInSqlite(priceValue);
-
+                priceValues.add(priceValue);
             }
+
+            Price.computeEvol(priceValues);
+            new StorePrice().storeInSqlite(priceValues);
 
         } catch (org.json.simple.parser.ParseException e) {
             e.printStackTrace();
@@ -144,13 +150,19 @@ public class BloombergProvider {
 
 
     public void getLastPrice(String isin, String ticker) {
+        // wait 1 second between request to avoid robot detection by bloomberg
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
         // Possible to use 1_WEEK, 1_MONTH, 1_YEAR,5_YEAR,MAX default is 1_WEEK
-        String baseUrl = "http://www.bloomberg.com/markets/api/bulk-time-series/price/" + ticker + "?timeFrame=1_MONTH";
+        String baseUrl = "https://www.bloomberg.com/markets/api/bulk-time-series/price/" + ticker + "?timeFrame=1_MONTH";
         Document doc = null;
         Elements priceElt = null;
         try {
-            String ua = "Mozilla/5.0 (Macintosh)";
-            doc = Jsoup.connect(baseUrl).validateTLSCertificates(false).ignoreContentType(true).userAgent(ua).get();
+            String ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:57.0) Gecko/20100101 Firefox/57.0";
+            doc = Jsoup.connect(baseUrl).validateTLSCertificates(false).ignoreContentType(true).userAgent(ua).timeout(50 * 1000).get();
             // System.out.println(doc.text());
         } catch (IOException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
