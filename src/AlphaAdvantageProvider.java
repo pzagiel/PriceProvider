0b26312b4bcc6f032ae6d8d0f8ca95ceee3d7d4f
pzagiel <patrick.zagiel@gmail.com>
2019-05-30 23:32:28 +0200

    Create AlphaAdvantageProvider.java
    New Provider AlphaAdvantge

---
+++ src/AlphaAdvantageProvider.java
@@ -0,0 +1,12 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: pzagiel
+ * Date: May 15, 2019
+ * Time: 1:59:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AlphaAdvantageProvider {
+
+    "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=ALGN&apikey=EZ49SSU14R2EGHNS"
+	"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=UMI.BR&apikey=EZ49SSU14R2EGHNS"
+}
