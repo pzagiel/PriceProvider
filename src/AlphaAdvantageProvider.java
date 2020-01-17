ae428628a4523dc0ca8c6d1f073961a7f567212a
pzagiel <patrick.zagiel@gmail.com>
2020-01-17 20:38:57 +0100

    ne w test

--- src/AlphaAdvantageProvider.java
+++ src/AlphaAdvantageProvider.java
@@ -7,6 +7,6 @@
  */
 public class AlphaAdvantageProvider {
 
-    "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=ALGN&apikey=EZ49SSU14R2EGHNS"
-	"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=UMI.BR&apikey=EZ49SSU14R2EGHNS"
+    // "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=ALGN&apikey=EZ49SSU14R2EGHNS"
+    //"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=UMI.BR&apikey=EZ49SSU14R2EGHNS"
 }
