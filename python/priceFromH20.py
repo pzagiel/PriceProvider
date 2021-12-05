282c944113d12aa390a8dd7af7671aa53eb38100
pzagiel <patrick.zagiel@gmail.com>
2021-12-05 16:30:33 +0100

    Update priceFromH20.py

--- python/priceFromH20.py
+++ python/priceFromH20.py
@@ -1,27 +1,39 @@
 import requests
 import json
 import time
+import sys
 import datetime
 import Price
+# Vivace,Vivace SP, Vivace HUSD,Vivace SP, Multistratgies, Multiequities,Multibonds,Allegro,Moderato 
+funds=["FR0011015478","FR0013535499","FR0012497980","FR0013535465","FR0010923383","FR0011008762","FR0010923375","FR0011015460","FR0010923367"]
 
-# Vivace, Vivace HUSD, Multistratgies, Multiequities,Multibonds,Allegro,Moderato
-funds=["FR0011015478","FR0012497980","FR0010923383","FR0011008762","FR0010923375","FR0011015460","FR0010923367"]
 for isin in funds:
-	url="https://www.h2o-am.com/wp-content/themes/amarou/hs/get_json.php?isin="+isin
+	# https seems not to work because of certificate verification failed so I change to http
+	url="http://www.h2o-am.com/wp-content/themes/amarou/hs/get_json.php?isin="+isin
+	#print url
 	r = requests.get(url)
+	# trick to avoid bug in first element making json parsing fail
+	# replace 0000 by 0
+	# Not necessary anymore bug corrected, JSON data OK
+	#data = json.loads(r.text.replace('[0000,','[0,'))
 	data = json.loads(r.text) 
-	#print isin+" "+time.strftime('%d-%m-%Y',time.localtime(int(data[len(data)-1][0])/1000))+" "+str(data[len(data)-1][1])
-	instrument = Price.INSTRUMENT()
+	instrument = Price.INSTRUMENT()	
+	priceNumber=len(data)-1 # last element
+
+	if (len(sys.argv)==2):
+		priceNumber=priceNumber+int(sys.argv[1]) # argument 1 is the offset from last price
+		
+	priceDate=datetime.datetime.utcfromtimestamp(data[priceNumber][0]/1000).replace(hour=0,minute=0, second=0, microsecond=0)
+	lastPrice=data[priceNumber][1]
+	prevPrice=data[priceNumber-1][1]
+	priceEvol=(lastPrice-prevPrice)/prevPrice
 	myPrice=Price.INSTR_PRICE(
-	                value_d=Price.INSTR_PRICE.convertDateToTime(datetime.datetime.utcfromtimestamp(data[len(data)-1][0]/1000).replace(hour=0,minute=0, second=0, microsecond=0)),
-	                value=data[len(data)-1][1],
-	                evol=(data[len(data)-1][1]-data[len(data)-2][1])/data[len(data)-2][1],
+	                value_d=Price.INSTR_PRICE.convertDateToTime(priceDate),
+	                value=lastPrice,
+	                evol=priceEvol,
 	                provider_id=10, # H20 Provider
 	                currency="EUR",
 	                instr_id=instrument.getId(isin)
 	                )
-	myPrice.store()
-	#Copy estimation for  Morningstar provider
-	myPrice.provider_id=1
 	myPrice.store()
 	print instrument.getName(isin)+" "+time.strftime('%d-%m-%Y',time.localtime(long(myPrice.value_d/1000)))+ " "+str(myPrice.value)+" "+str(round(myPrice.evol,4)*100)+"%"
