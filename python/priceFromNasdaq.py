938077ef3e2ae9bf7d332b4f0374483ef4c47c4d
pzagiel <patrick.zagiel@gmail.com>
2023-03-27 18:59:15 +0200

    Nasdaq data provider

---
+++ python/priceFromNasdaq.py
@@ -0,0 +1,3 @@
+#https://www.nasdaq.com/market-activity/index/nqcybrt
+#https://api.nasdaq.com/api/quote/NQCYBRT/chart?assetclass=index&fromdate=2022-05-04&todate=2022-11-04
+#https://api.nasdaq.com/api/quote/PANW/historical?assetclass=stocks&fromdate=2022-10-01&limit=1000&todate=2022-11-04 # Palo Alto Stock
