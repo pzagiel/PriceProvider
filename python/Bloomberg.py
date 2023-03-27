e212f04a3c155809a4ba98406550eb414b922a09
pzagiel <patrick.zagiel@gmail.com>
2023-03-27 18:56:27 +0200

    Bloomberg data provider

---
+++ python/Bloomberg.py
@@ -0,0 +1,28 @@
+Price From Bloomberg
+
+https://github.com/matthewfieger/bloomberg_stock_data
+
+https://stackoverflow.com/questions/58064494/scrap-data-from-bloomberg
+
+https://www.bloomberg.com/markets2/api/history/ALGN:US/PX_LAST?timeframe=6_MONTH&period=daily&volumePeriod=daily
+https://www.bloomberg.com/markets2/api/history/ALGN%3AUS/PX_LAST?timeframe=5_YEAR&period=weekly&volumePeriod=weekly
+# really Give price for 5 YEAR Period daily
+https://www.bloomberg.com/markets2/api/history/ALGN%3AUS/PX_LAST?timeframe=5_YEAR&period=daily
+# Works ALso
+https://www.bloomberg.com/markets/api/bulk-time-series/price/ALGN:US?timeFrame=1_YEAR&period=daily
+
+https://www.bloomberg.com/markets/api/bulk-time-series/price/GT10:GOV?timeFrame=1_YEAR&period=daily
+
+https://www.bloomberg.com/markets2/api/history/FTSEMIB:IND/PX_LAST?timeframe=6_MONTH&period=daily
+
+https://www.bloomberg.com/markets/api/bulk-time-series/price/CL1:COM?timeFrame=1_MONTH
+
+https://www.bloomberg.com/markets2/api/history/CL1:COM/PX_LAST?timeframe=1_YEAR&period=daily
+
+https://www.bloomberg.com/markets/api/bulk-time-series/price/OLBP:LN?timeFrame=1_MONTH
+
+
+
+
+
+
