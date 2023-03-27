93abcf3469ea4f8a7294d25f1a814368dcb80974
pzagiel <patrick.zagiel@gmail.com>
2023-03-27 19:00:49 +0200

    MSCI data provider

---
+++ python/MSCI.py
@@ -0,0 +1,5 @@
+
+
+# URL Example
+# format XLS,XML,CSV
+https://app2.msci.com/webapp/indexperf/charts?indices=2591,C,36&startDate=04%20Feb,%202016&endDate=04%20Feb,%202020&priceLevel=41&currency=15&frequency=D&scope=R&format=XLS&baseValue=false&site=gimi
